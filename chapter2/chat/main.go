7a0b2a0ba3ad513755e6c99d63f6de2d0a44aecc
y.urakawa <s08a2015@gmail.com>
2018-03-17 18:26:03 +0900

    [p35]added sign in auth

--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -9,6 +9,11 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 	"github.com/yurakawa/goblueprints/chapter2/trace"
 )
 
@@ -24,7 +29,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	err := t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	err := t.templ.Execute(w, data)
 	if err != nil {
 		panic(err)
 	}
@@ -34,11 +46,20 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈する
 
+	// Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("セキュリティキー")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の鍵", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密の鍵", "http://localhost:8080/auth/callback/github"),
+		google.New("クライアントID", "秘密の鍵", "http://localhost:8080/auth/callback/google"),
+	)
+
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	//http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+
 	http.Handle("/room", r)
 
 	// チャットルームを開始する
