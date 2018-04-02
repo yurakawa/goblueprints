8b1cce484fe28c81cd8812cc685f3a41e1e09fb1
y.urakawa <s08a2015@gmail.com>
2018-04-02 10:11:22 +0900

    [p96] 受け取った文字列をドメイン名として適切なものに変換して末尾にTLDを追加するdomainifyを実装

---
+++ chapter4/domainify/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+	"unicode"
+)
+
+var tlds = []string{"com", "net"}
+
+const allowedCharas = "abcdefghijklmnopqrstuvwxyz0123456789_-"
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+
+	for s.Scan() {
+		text := strings.ToLower(s.Text())
+		var newText []rune
+		for _, r := range text {
+			if unicode.IsSpace(r) {
+				r = '-'
+			}
+			if !strings.ContainsRune(allowedCharas, r) {
+				continue
+			}
+			newText = append(newText, r)
+		}
+		fmt.Println(string(newText) + "." + tlds[rand.Intn(len(tlds))])
+	}
+}
