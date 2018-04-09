ff6fe2bbeda935b124e667e8e6833a60f9b668d4
y.urakawa <s08a2015@gmail.com>
2018-04-10 00:41:40 +0900

    [p125] Twitterを使った認証

---
+++ chapter5/socialpool/twittervotes/twitter.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+
+	"net/http"
+	"net/url"
+	"sync"
+
+	"strconv"
+
+	"github.com/joeshaw/envdecode"
+	"github.com/matryer/go-oauth/oauth"
+)
+
+var conn net.Conn
+
+// 接続が閉じられているか確認し新しい接続を開く
+func dial(netw, addr string) (net.Conn, error) {
+	if conn != nil {
+		conn.Close()
+		conn = nil
+	}
+	netc, err := net.DialTimeout(netw, addr, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	conn = netc
+	return netc, nil
+}
+
+var reader io.ReadCloser
+
+// 現在の接続を切断してクリーンアップを行う
+func closeConn() {
+	if conn != nil {
+		conn.Close()
+	}
+	if reader != nil {
+		reader.Close()
+	}
+}
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+// 環境変数を読み込んでOAuth オブジェクトをセットアップする
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY,required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN,required"`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET,required"`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+}
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
+
+// 認証されたリクエストを作成しレスポンスを受け取る
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+	return httpClient.Do(req)
+}
