80f0827883fa1b388240278da9b762fe7a79e811
y.urakawa <s08a2015@gmail.com>
2018-04-19 00:19:56 +0900

    [p154] ラップされたハンドラ関数

---
+++ chapter6/socialpool/api/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+)
+
+func main() {}
+
+// withAPIKey 引数も戻り値もhttp.HandlerFunc型。「コンテキストの中にラップする」
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !isValidAPIKey(r.URL.Query().Get("key")) {
+			respondErr(w, r, http.StatusUnauthorized, "不正なAPIキーです")
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
+}
+
+func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		thisDb := d.Copy()
+		defer thisDb.Close()
+		// ballotsデータベースへの参照をdb変数にセット
+		SetVar(r, "db", thisDb.DB("ballots"))
+		f(w, r)
+	}
+}
+
+func withVars(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		OpenVars(r)
+		defer CloseVars(r)
+		fn(w, r)
+	}
+}
+
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	// TODO: https://github.com/fasterness/cors を使う
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
+}
