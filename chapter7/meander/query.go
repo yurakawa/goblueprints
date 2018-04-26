f68e595fd2f1a01f93ae4a5d3d158824d163c07a
y.urakawa <s08a2015@gmail.com>
2018-04-27 04:42:06 +0900

    [p194] Add function to generate recommendation

--- chapter7/meander/query.go
+++ chapter7/meander/query.go
@@ -1,5 +1,16 @@
 package meander
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
+)
+
 // APIKey is the Google Places API key that will be used
 // to make requests to the service.
 var APIKey string
@@ -41,4 +52,74 @@ func (p *Place) Public() interface{} {
 		"lat":      p.Lat,
 		"lng":      p.Lng,
 	}
+}
+
+// Query represents a runnable Query against the
+// Google Places API.
+type Query struct {
+	Lat          float64
+	Lng          float64
+	Journey      []string
+	Radius       int
+	CostRangeStr string
+}
+
+func (q *Query) find(types string) (*googleResponse, error) {
+	u := "http://maps.googleapis.com/maps/api/place/nearbysearch/json"
+	vals := make(url.Values)
+	vals.Set("location", fmt.Sprintf("%g,%g", q.Lat, q.Lng))
+	vals.Set("radius", fmt.Sprintf("%d", q.Radius))
+	vals.Set("types", types)
+	vals.Set("key", APIKey)
+	if len(q.CostRangeStr) > 0 {
+		r := ParseCostRange(q.CostRangeStr)
+		vals.Set("minprice", fmt.Sprintf("%d", int(r.From)-1))
+		vals.Set("maxprice", fmt.Sprintf("%d", int(r.To)-1))
+	}
+	res, err := http.Get(u + "?" + vals.Encode())
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var response googleResponse
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+// Run 問い合わせを一斉に行いその結果を返す
+func (q *Query) Run() []interface{} {
+	rand.Seed(time.Now().UnixNano())
+	var w sync.WaitGroup
+	var l sync.Mutex
+	places := make([]interface{}, len(q.Journey))
+	for i, r := range q.Journey {
+		w.Add(1)
+		go func(types string, i int) {
+			defer w.Done()
+			response, err := q.find(types)
+			if err != nil {
+				log.Println("施設の検索に失敗しました：", err)
+				return
+			}
+			if len(response.Results) == 0 {
+				log.Fatal("施設が見つかりませんでした：", err)
+				return
+			}
+			for _, result := range response.Results {
+				for _, photo := range result.Photos {
+					photo.URL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photoreference=" +
+						photo.PhotoRef + "&key=" + APIKey
+
+				}
+			}
+			randI := rand.Intn(len(response.Results))
+			l.Lock()
+			places[i] = response.Results[randI]
+			l.Unlock()
+		}(r, i)
+	}
+	w.Wait() // すべてのリクエストの完了を待つ
+	return places
 }
