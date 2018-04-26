e02b01c0ef01920e653dc2666cfe7e47717543e2
y.urakawa <s08a2015@gmail.com>
2018-04-27 05:01:38 +0900

    [p195] /recommendations の URLパラメータを解釈するハンドラを追加

--- chapter7/meander/cmd/main.go
+++ chapter7/meander/cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"runtime"
+	"strconv"
+	"strings"
 
 	"github.com/yurakawa/goblueprints/chapter7/meander"
 )
@@ -16,6 +18,19 @@ func main() {
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
 	})
+
+	http.HandleFunc("/recommendations", func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("Journey"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	})
+
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
 
