bfb45cb446584fc6a53ff9d7a17a68dc00484995
y.urakawa <s08a2015@gmail.com>
2018-05-05 15:18:48 +0900

    [p211] Decide the file name format in Archiver. It is not safe to use non-constant format strings

--- chapter8/backup/archiver.go
+++ chapter8/backup/archiver.go
@@ -2,6 +2,7 @@ package backup
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -10,13 +11,17 @@ import (
 // Archiver represents type capable of archiving and
 // restoring files.
 type Archiver interface {
+	DestFmt() func(int64) string
 	Archive(src, dest string) error
 }
 
 type zipper struct{}
 
-// ZIP はファイルの圧縮とその解除にZIP形式を利用するArchiverです。
-var ZIP Archiver = (*zipper)(nil)
+func (z *zipper) DestFmt() func(int64) string {
+	return func(i int64) string {
+		return fmt.Sprintf("%d.zip", i)
+	}
+}
 
 func (z *zipper) Archive(src, dest string) error {
 	if err := os.MkdirAll(filepath.Dir(dest), 0777); err != nil {
@@ -47,3 +52,6 @@ func (z *zipper) Archive(src, dest string) error {
 		return nil
 	})
 }
+
+// ZIP はファイルの圧縮とその解除にZIP形式を利用するArchiverです。
+var ZIP Archiver = (*zipper)(nil)
